a8f75eb9bd3b7178765724ac88ef8f5fcdbfc00c
Fantix King <fantix.king@gmail.com>
2017-07-24 19:43:33 +0800

    Fixed #15, delegated more asyncpg API

--- examples/delegated.py
+++ examples/delegated.py
@@ -18,10 +18,19 @@ async def main():
     async with db.create_pool('postgresql://localhost/gino') as pool:
         # You will need to create the database and table manually
 
+        for u in await pool.fetch('SELECT * FROM users WHERE id > $1', 3):
+            print(u)
         for u in await pool.fetch(User.query.where(User.id > 3)):
             print(u)
         for u in await pool.fetch(select([User.id])):
             print(u)
+        u = await pool.fetchrow(User.query.where(User.id > 1))
+        print(u)
+        nickname = await pool.fetchval(
+            User.select('nickname').where(User.id == 1))
+        print(nickname)
+        status = await pool.execute(User.query)
+        print(status)
         async with pool.acquire() as conn:
             async with conn.transaction():
                 async for u in conn.cursor(User.query.where(User.id > 3)):
--- gino/asyncpg_delegate.py
+++ gino/asyncpg_delegate.py
@@ -28,22 +28,55 @@ class GinoPool(Pool):
 class GinoConnection(Connection):
     metadata = None
 
-    async def fetch(self, query, *args, timeout=None):
+    async def _execute(self, query, args, limit, timeout, return_status=False,
+                       guess=True, **kwargs):
         if isinstance(query, str):
-            return super().fetch(query, *args, timeout=timeout)
-        model = self.metadata.guess_model(query)
-        query, params = self.metadata.compile(query)
-        rv = await super().fetch(query, *params, timeout=timeout)
+            return await super()._execute(query, args, limit, timeout,
+                                          return_status)
+
+        model = self.metadata.guess_model(query) if guess else None
+        query, params = self.metadata.compile(query, *args, **kwargs)
+        rv = await super()._execute(query, params, limit, timeout,
+                                    return_status)
         if model is not None:
+            if return_status:
+                rv, status, completed = rv
             rv = list(map(model.from_row, rv))
+            if return_status:
+                # noinspection PyUnboundLocalVariable
+                rv = rv, status, completed
         return rv
 
-    def cursor(self, query, *args, timeout=None):
+    async def fetchval(self, query, *args, column=0, timeout=None):
+        self._check_open()
+        data = await self._execute(query, args, 1, timeout, guess=False)
+        if not data:
+            return None
+        return data[0][column]
+
+    async def execute(self, query, *args, timeout: float=None,
+                      **kwargs) -> str:
         if isinstance(query, str):
-            return super().cursor(query, *args, timeout=timeout)
+            return await super().execute(query, *args, timeout=timeout)
+
+        self._check_open()
+        query, params = self.metadata.compile(query, *args, **kwargs)
+
+        if not params:
+            return await self._protocol.query(query, timeout)
+
+        _, status, _ = await super()._execute(query, args, 0, timeout, True)
+        return status.decode()
+
+    def cursor(self, query, *args, prefetch=None, timeout=None, **kwargs):
+        if isinstance(query, str):
+            return super().cursor(query, *args,
+                                  prefetch=prefetch, timeout=timeout)
+
+        self._check_open()
         model = self.metadata.guess_model(query)
-        query, params = self.metadata.compile(query)
-        rv = super().cursor(query, *params)
+        query, params = self.metadata.compile(query, *args, **kwargs)
+        rv = super().cursor(query, *params, prefetch=prefetch, timeout=timeout)
         if model is not None:
             rv = model.map(rv)
         return rv
