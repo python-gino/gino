4584c8c6001b29358f866f69932e42427f3b749d
Fantix King <fantix.king@gmail.com>
2017-07-28 15:07:02 +0800

    Bump version: 0.2.0 → 0.2.1

--- gino/__init__.py
+++ gino/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Fantix King"""
 __email__ = 'fantix.king@gmail.com'
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from .declarative import Gino
 from .asyncpg_delegate import GinoConnection
