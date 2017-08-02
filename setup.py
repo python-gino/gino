d12d8992403112a92f51ce6d11c81712c59d537b
Fantix King <fantix.king@gmail.com>
2017-08-02 10:01:44 +0800

    Bump version: 0.2.1 → 0.2.2

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ test_requirements = [
 
 setup(
     name='gino',
-    version='0.2.1',
+    version='0.2.2',
     description="GINO Is Not ORM - "
                 "a Python ORM on asyncpg and SQLAlchemy core.",
     long_description=readme + '\n\n' + history,
