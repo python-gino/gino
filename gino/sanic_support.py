ee16b87ac84c4ba11e41bf8a5d9e059240ef5f89
lbhsot <lbhsot@163.com>
2017-08-14 10:35:44 +0800

    refs #30 remove LazyTransactionalConnection and add LazyConnection.

--- gino/sanic_support.py
+++ gino/sanic_support.py
@@ -1,55 +1,46 @@
-from .local import get_local
 from .local import enable_task_local, disable_task_local
 
 
-class LazyTransactionalConnection:
-    def __init__(self, metadata):
-        self._metadata = metadata
-        self._ctx = self
-
-    async def get_bind(self):
-        if self._ctx is None:
-            self._ctx = self._metadata.transaction()
-            await self._ctx.__aenter__()
-        return self._ctx.connection
-
-    async def exit(self, *args):
-        if self._ctx is not None:
-            ctx, self._ctx = self._ctx, None
-            await ctx.__aexit__(*args)
-
-
 class SanicMixin:
+    """
+    Enable lazy connection for request by default.
+    You can use 'app.config[DB_USE_CONNECTION_FOR_REQUEST] = False' to change
+    the default value.
+    Database connections will be auto closed when on_response().
+    simple usages:
+        # simple get user
+        await User.get(11)
+        # close database connection by manual when needed.
+        await request['connection'].close()
+    """
     def init_app(self, app):
-        @app.middleware('request')
-        async def on_request(request):
-            print('on_request')
-            local = get_local()
-            if local:
-                stack = local.get('connection_stack')
-                if stack:
-                    stack.append(LazyTransactionalConnection(self))
+        if app.config.setdefault('DB_USE_CONNECTION_FOR_REQUEST', True):
+            @app.middleware('request')
+            async def on_request(request):
+                # noinspection PyUnresolvedReferences
+                request['conn_ctx'] = ctx = self.acquire(lazy=True)
+                request['connection'] = await ctx.__aenter__()
 
-        @app.middleware('response')
-        async def on_response(request, response):
-            print('on_response')
-            local = get_local()
-            if local:
-                stack = local.get('connection_stack')
-                if stack:
-                    ltc = stack.pop()
-                    await ltc.exit(None if response.status == 200 else True,
-                                   None, None)
+            @app.middleware('response')
+            async def on_response(request, response):
+                ctx = request.pop('conn_ctx', None)
+                request.pop('connection', None)
+                if ctx is not None:
+                    await ctx.__aexit__(None, None, None)
 
         @app.listener('before_server_start')
         async def before_server_start(_, loop):
+            # await User.get(8)
+            # await request['connection'].close()
+
             enable_task_local(loop)
+            # noinspection PyUnresolvedReferences
             await self.create_pool(
-                host=app.config.setdefault('GINO_HOST', 'localhost'),
-                port=app.config.setdefault('GINO_PORT', 5432),
-                user=app.config.setdefault('GINO_USER', 'postgres'),
-                password=app.config.setdefault('GINO_PASSWORD', ''),
-                database=app.config.setdefault('GINO_DATABASE', 'postgres'),
+                host=app.config.setdefault('DB_HOST', 'localhost'),
+                port=app.config.setdefault('DB_PORT', 5432),
+                user=app.config.setdefault('DB_USER', 'postgres'),
+                password=app.config.setdefault('DB_PASSWORD', ''),
+                database=app.config.setdefault('DB_DATABASE', 'postgres'),
                 min_size=app.config.setdefault('DB_POOL_MIN_SIZE', 5),
                 max_size=app.config.setdefault('DB_POOL_MAX_SIZE', 10),
                 loop=loop,
@@ -57,5 +48,6 @@ class SanicMixin:
 
         @app.listener('after_server_stop')
         async def after_server_stop(_, loop):
+            # noinspection PyUnresolvedReferences
             await self.bind.close()
             disable_task_local(loop)
