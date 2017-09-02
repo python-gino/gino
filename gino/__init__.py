f4969875ca7ff79b72fdf53a48ebbe3a82930e9b
Fantix King <fantix.king@gmail.com>
2017-09-02 17:22:03 +0800

    Fix bumpversion

--- gino/__init__.py
+++ gino/__init__.py
@@ -3,3 +3,5 @@ from .pool import GinoPool
 from .connection import GinoConnection
 from .local import get_local, enable_task_local, disable_task_local
 from .exceptions import *
+
+__version__ = '0.4.1'
