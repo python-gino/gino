82308a908e36ee5c762ecdd8e72163b2b80de69a
neal <qdzzyb2015@gmail.com>
2017-09-25 13:21:18 +0800

    user set replace tuple

--- gino/declarative.py
+++ gino/declarative.py
@@ -29,7 +29,7 @@ class ModelType(type):
 
     def __getattr__(self, item):
         try:
-            if item in ('insert',):
+            if item in {'insert'}:
                 return getattr(self.__table__, item)
             raise AttributeError
         except AttributeError:
