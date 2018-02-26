a276502aad947179d7fa0db00c064bb85f6671a1
Fantix King <fantix.king@gmail.com>
2018-02-26 21:11:13 +0800

    License year

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'GINO'
-copyright = u"2017, Fantix King"
+copyright = u"2017-present, Fantix King"
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
