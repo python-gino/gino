f307bf4ff035a4995557ddc531900a40d465daea
Tony Wang <wwwjfy@gmail.com>
2018-04-01 12:37:48 +0800

    enforce utf-8 on setup.py
    in the non utf-8 environment, e.g. LANG=C, reading HISTORY.rst would
    cause UnicodeDecodeError

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.rst', encoding='utf-8') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
+with open('HISTORY.rst', encoding='utf-8') as history_file:
     history = history_file.read()
 
 
