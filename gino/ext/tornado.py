e9e3523c88b7a7beb28d79cfe733224d46edfb36
Vladimir Goncharov <amatanhead@yandex-team.ru>
2018-04-01 21:31:27 +0300

    Update tornado extension to support tornado>=5.0.1

--- gino/ext/tornado.py
+++ gino/ext/tornado.py
@@ -1,164 +1,18 @@
-"""
-GINO provides a convenient plugin for integrating with Tornado_ webserver.
-It consists of two parts, each of them is optional.
-
-.. _Tornado: http://www.tornadoweb.org
-
-.. warning::
-
-    Tornado doesn't wrap request handlers to asyncio tasks, hence
-    task locals doesn't work in request handlers by default. To fix this,
-    you may either redefine ``_execute()`` method on you handlers to wrap
-    request processing into a task, or simply use
-    :py:class:`gino.ext.tornado.AsyncioRequestHandler`
-    as a base class for all of your handlers.
-
-    See `integrate GINO with application and request handlers`_ for more details.
-
-
-Provide tornado-specific methods on models
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-GINO can provide a webframework-aware ``.get_or_404()`` method which work
-similar to ``.get()`` but raises an appropriate error whenever requested object
-not found. In case of tornado, an appropriate error is
-``tornado.web.HTTPError(404)``.
-
-To have it working, simply use :py:class:`gino.ext.tornado.Gino` as your
-database metadata.
-
-
-Integrate GINO with application and request handlers
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-In addition to ``.get_or_404()``, GINO provides bases for application and
-request handler objects.
-
-Inherit your application class from :py:class:`gino.ext.tornado.Application`
-to automate connection pool management and provide access to the database
-object to all of your request handlers via ``self.application.db``.
-
-Inherit your request handlers from
-:py:class:`gino.ext.tornado.AsyncioRequestHandler` to enable task locals
-support.
-
-Inherit your request handlers from :py:class:`gino.ext.tornado.GinoRequestHandler`
-to enable active connection management.
-Note that :py:class:`gino.ext.tornado.GinoRequestHandler` requires your application
-to have a ``db`` property with ``acquire`` coroutine so its best to use it
-with :py:class:`gino.ext.tornado.Application`.
-
-
-Settings defined by this extension
-^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-GINO would define some options for database configuration. Use them with
-the standard ``tornado.options`` module:
-
-- ``'db_driver'`` -- if not set, ``asyncpg``;
-- ``'db_host'`` -- if not set, ``localhost``;
-- ``'db_port'`` -- if not set, ``5432``;
-- ``'db_user'`` -- if not set, ``postgres``;
-- ``'db_password'`` -- if not set, empty string;
-- ``'db_database'`` -- if not set, ``postgres``;
-- ``'db_pool_min_size'`` -- number of connection the pool will be initialized
-  with. Default is ``5``;
-- ``'db_pool_max_size'`` -- max number of connections in the pool.
-  Default is ``10``;
-- ``'db_pool_max_inactive_conn_lifetime'`` -- number of seconds after which
-  inactive connections in the pool will be closed.  Pass ``0`` to disable this
-  mechanism. Default is ``300``;
-- ``'db_pool_max_queries '`` -- number of queries after a connection is closed
-  and replaced with a new connection. Default is ``50000``.
-
-
-An example application
-^^^^^^^^^^^^^^^^^^^^^^
-
-A helloworld application that uses tornado and GINO may look like this:
-
-.. code-block:: python
-
-    import tornado.web
-    import tornado.ioloop
-    import tornado.options
-    import tornado.escape
-
-    from gino.ext.tornado import Gino, Application, GinoRequestHandler
-
-
-    # Define your database metadata
-    # -----------------------------
-
-    db = Gino()
-
-
-    # Define tables as you would normally do
-    # --------------------------------------
-
-    class User(db.Model):
-        __tablename__ = 'users'
-
-        id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
-        nickname = db.Column(db.Unicode(), nullable=False)
-
-
-    # Now just use your tables
-    # ------------------------
-
-    class AllUsers(GinoRequestHandler):
-        async def get(self):
-            users = await User.query.gino.all()
-
-            for user in users:
-                url = self.application.reverse_url('user', user.id)
-                nickname = tornado.escape.xhtml_escape(user.nickname)
-                self.write(f'<a href="{url}">{nickname}</a><br/>')
-
-
-    class GetUser(GinoRequestHandler):
-        async def get(self, uid):
-            user: User = await User.get_or_404(int(uid))
-            self.write(f'Hi, {user.nickname}!')
-
-
-    if __name__ == '__main__':
-        tornado.options.parse_command_line()
-        tornado.ioloop.IOLoop.configure('tornado.platform.asyncio.AsyncIOMainLoop')
-
-        app = Application([
-            tornado.web.URLSpec(r'/', AllUsers, name='index'),
-            tornado.web.URLSpec(r'/user/(?P<uid>[0-9]+)', GetUser, name='user')
-        ], debug=True)
-
-        loop = tornado.ioloop.IOLoop.current().asyncio_loop
-
-        # If you intend to use HTTPServer in multiprocessed environment,
-        # call the app.late_init method after calling HTTPServer.start(n).
-        # This will create one connection pool per process.
-        loop.run_until_complete(app.late_init(db))
-
-        app.listen(8888)
-
-        loop.run_forever()
-
-API reference
-^^^^^^^^^^^^^
-
-"""
-
 import asyncio
-import sys
 import typing
 
+import tornado
 import tornado.ioloop
 import tornado.iostream
 import tornado.log
 import tornado.platform.asyncio
 import tornado.web
+import tornado.gen
+
+from tornado.options import options as _options, define as _define
 
 from sqlalchemy.engine.url import URL
-from tornado.options import options as _options, define as _define
+
 try:
     # noinspection PyPackageRequirements
     from aiocontextvars import enable_inherit as _enable_inherit
@@ -167,7 +21,10 @@ except ImportError:
 
 from ..api import Gino as _Gino, GinoExecutor as _Executor
 from ..engine import GinoConnection as _Connection, GinoEngine as _Engine
-from ..strategies import GinoStrategy
+from ..strategies import GinoStrategy as _GinoStrategy
+
+
+__all__ = ['Gino', 'Application', 'GinoRequestHandler']
 
 
 def _assert_not_negative(name):
@@ -232,7 +89,7 @@ class GinoEngine(_Engine):
         return rv
 
 
-class TornadoStrategy(GinoStrategy):
+class TornadoStrategy(_GinoStrategy):
     name = 'tornado'
     engine_cls = GinoEngine
 
@@ -258,7 +115,12 @@ class Gino(_Gino):
         from ..crud import CRUDModel as __CRUDModel
         from ..declarative import ModelType as __ModelType
 
-        class Model(__CRUDModel, TornadoModelMixin, metaclass=__ModelType):
+        class _Model(__CRUDModel, TornadoModelMixin, metaclass=__ModelType):
+            ...
+
+        # noinspection PyPropertyDefinition
+        @property
+        def Model(self) -> _Model:
             ...
 
     async def first_or_404(self, *args, **kwargs):
@@ -289,7 +151,8 @@ class Application(tornado.web.Application):
     #: for more info.
     use_connection_for_request: bool = True
 
-    async def late_init(self, db: Gino, *, loop=None, options=_options):
+    async def late_init(self, db: Gino, *, loop=None, options=_options,
+                        additional_options=None):
         """
         Initialize this application with a database object.
 
@@ -302,13 +165,18 @@ class Application(tornado.web.Application):
 
         :param db: the :py:class:`gino.ext.tornado.Gino()` class instance that
             will be used in this application.
-        :param loop: io loop that will be used to run heep server, either
+        :param loop: io loop that will be used to run http server, either
             tornado's or asyncio's.
         :param options: a tornado's ``OptionParser()`` instance or any
             dictionary-like object with the database settings. Default is to
             use ``tornado.options.options`` global.
+        :param additional_options: dict of additional options that will
+            be passed directly to the :py:meth:`~.Gino.set_bind()`.
 
         """
+
+        if tornado.version_info < (5, 0, 1):
+            raise RuntimeError('tornado>=5.0.1 is required to run GINO')
 
         if loop is None:
             loop = tornado.ioloop.IOLoop.current()
@@ -322,6 +190,7 @@ class Application(tornado.web.Application):
         _enable_inherit(asyncio_loop)
 
         self.db: Gino = db
+
         await db.set_bind(
             URL(
                 drivername=options['db_driver'],
@@ -337,108 +206,17 @@ class Application(tornado.web.Application):
                 options['db_pool_max_inactive_conn_lifetime']
             ),
             max_queries=options['db_pool_max_queries'],
-            loop=asyncio_loop
+            loop=asyncio_loop,
+            **(additional_options or {})
         )
 
 
 # noinspection PyAbstractClass
-class AsyncioRequestHandler(tornado.web.RequestHandler):
-    """
-    This class enables support for task locals by wrapping the ``_execute()``
-    method into ``asyncio.Task`` instances.
-
-    """
-
-    application: tornado.web.Application
-
-    async def _setup_connection(self):
-        """
-        Hook for creating connection.
-
-        """
-        pass
-
-    async def _teardown_connection(self):
-        """
-        Hook for destroying connection.
-
-        """
-        pass
-
-    def _execute(self, transforms, *args, **kwargs):
-        loop = tornado.ioloop.IOLoop.current()
-
-        if not isinstance(loop, tornado.platform.asyncio.BaseAsyncIOLoop):
-            raise RuntimeError('AsyncIOLoop is required to run GINO')
-
-        asyncio_loop = loop.asyncio_loop
-
-        return asyncio.ensure_future(
-            self._do_execute(transforms, *args, **kwargs),
-            loop=asyncio_loop
-        )
-
-    async def _do_execute(self, transforms, *args, **kwargs):
-        """
-        An actual asyncio-compatible implementation on the ``_execute``.
-
-        This function just takes the original generator ``_execute.__wrapped__``
-        and manages to pass its futures to the underlying asyncio loop.
-
-        It also calls ``_setup_connection`` and ``_teardown_connection``
-        methods and manages all errors that happen there.
-
-        """
-
-        self._transforms = transforms
-
-        try:
-            gen = super()._execute.__wrapped__(
-                self, transforms, *args, **kwargs
-            )
-
-            data = None
-            exc_info = None
-
-            await self._setup_connection()
-
-            try:
-                while True:
-                    try:
-                        if exc_info is None:
-                            coro = gen.send(data)
-                        else:
-                            coro = gen.throw(*exc_info)
-                    except StopIteration:
-                        return
-                    exc_info = None
-                    data = None
-                    # noinspection PyBroadException
-                    try:
-                        data = await coro
-                    except:
-                        exc_info = sys.exc_info()
-            finally:
-                await self._teardown_connection()
-        except Exception as e:
-            # noinspection PyBroadException
-            try:
-                self._handle_request_exception(e)
-            except Exception:
-                tornado.log.app_log.exception("exception in exception handler")
-        finally:
-            if self._prepared_future is not None:
-                if not self._prepared_future.done():
-                    self._prepared_future.set_result(None)
-
-
-# noinspection PyAbstractClass
-class GinoRequestHandler(AsyncioRequestHandler):
+class GinoRequestHandler(tornado.web.RequestHandler):
     """
     Base class for all request handlers that use GINO.
 
-    In addition to features provided by :py:class:`~.AsyncioRequestHandler`,
-    this class manages lazy connections for each request.
+    Manages lazy connections for each request.
 
     """
 
@@ -474,15 +252,6 @@ class GinoRequestHandler(AsyncioRequestHandler):
         """
         return self.application.db
 
-    async def _setup_connection(self):
-        if self.use_connection_for_request:
-            self.__db_connection = await self.db.acquire(lazy=True)
-
-    async def _teardown_connection(self):
-        if self.__db_connection is not None:
-            await self.__db_connection.release()
-            self.__db_connection = None
-
     @property
     def db_connection(self):
         """
@@ -498,3 +267,45 @@ class GinoRequestHandler(AsyncioRequestHandler):
 
         """
         await self._teardown_connection()
+
+    async def _setup_connection(self):
+        """
+        Hook for creating connection.
+
+        """
+        # Ensure that we have a current task assigned. Otherwise,
+        # lazy connections may not work properly.
+        assert asyncio.Task.current_task() is not None
+
+        if self.use_connection_for_request:
+            self.__db_connection = await self.db.acquire(lazy=True)
+
+    async def _teardown_connection(self):
+        """
+        Hook for destroying connection.
+
+        """
+
+        if self.__db_connection is not None:
+            await self.__db_connection.release()
+            self.__db_connection = None
+
+    # noinspection PyBroadException
+    @tornado.gen.coroutine
+    def _execute(self, *args, **kwargs):
+        try:
+            try:
+                yield self._setup_connection()
+                yield from super(GinoRequestHandler, self)._execute(*args, **kwargs)
+            finally:
+                yield self._teardown_connection()
+        except Exception as e:
+            tornado.log.app_log.exception('exception during lazy connection '
+                                          'setup/teardown')
+            try:
+                self._handle_request_exception(e)
+            except Exception:
+                tornado.log.app_log.exception('exception in exception handler')
+            if self._prepared_future is not None:
+                if not self._prepared_future.done():
+                    self._prepared_future.set_result(None)
--- tests/test_tornado.py
+++ tests/test_tornado.py
@@ -1,6 +1,7 @@
 import pytest
 import tornado.web
 import tornado.httpclient
+import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.escape
@@ -11,14 +12,8 @@ from .models import DB_ARGS
 
 # noinspection PyShadowingNames
 @pytest.fixture
-def app():
-    # Define your database metadata
-    # -----------------------------
-
+async def app():
     db = Gino()
-
-    # Define tables as you would normally do
-    # --------------------------------------
 
     class User(db.Model):
         __tablename__ = 'users'
@@ -26,11 +21,11 @@ def app():
         id = db.Column(db.Integer(), primary_key=True, autoincrement=True)
         nickname = db.Column(db.Unicode(), nullable=False)
 
-    # Now just use your tables
-    # ------------------------
-
     class AllUsers(GinoRequestHandler):
         async def get(self):
+            import asyncio
+            assert asyncio.Task.current_task() is not None
+
             users = await User.query.gino.all()
 
             for user in users:
@@ -41,12 +36,14 @@ def app():
     class GetUser(GinoRequestHandler):
         async def get(self, uid):
             user: User = await User.get_or_404(int(uid))
-            self.write(f'Hi, {user.nickname}!')
+            nickname = tornado.escape.xhtml_escape(user.nickname)
+            self.write(f'Hi, {nickname}!')
 
     class AddUser(GinoRequestHandler):
         async def post(self):
             user = await User.create(nickname=self.get_argument('name'))
-            self.write(f'Hi, {user.nickname}!')
+            nickname = tornado.escape.xhtml_escape(user.nickname)
+            self.write(f'Hi, {nickname}!')
 
     options = {
         'db_host': DB_ARGS['host'],
@@ -62,38 +59,49 @@ def app():
         tornado.web.URLSpec(r'/user/(?P<uid>[0-9]+)', GetUser, name='user'),
         tornado.web.URLSpec(r'/user', AddUser, name='user_add'),
     ], debug=True)
-    loop = tornado.ioloop.IOLoop.current().asyncio_loop
-    loop.run_until_complete(app.late_init(db))
-    loop.run_until_complete(db.gino.create_all())
+    await app.late_init(db)
+    await db.gino.create_all()
     try:
         yield app
     finally:
-        loop.run_until_complete(db.gino.drop_all())
+        await db.gino.drop_all()
 
 
+# noinspection PyShadowingNames
 @pytest.fixture
-async def io_loop(event_loop):
-    from tornado.platform.asyncio import AsyncIOMainLoop
-    return AsyncIOMainLoop()
+async def base_url(app: Application, unused_tcp_port):
+    server = tornado.httpserver.HTTPServer(app)
+    server.listen(unused_tcp_port)
+    try:
+        yield f'http://localhost:{unused_tcp_port}'
+    finally:
+        server.stop()
 
 
-@pytest.mark.gen_test
-def test_hello_world(http_client, base_url):
-    response = yield http_client.fetch(base_url)
+# noinspection PyShadowingNames
+@pytest.fixture
+def http_client():
+    return tornado.httpclient.AsyncHTTPClient()
+
+
+# noinspection PyShadowingNames
+@pytest.mark.asyncio
+async def test_hello_world(http_client, base_url):
+    response = await http_client.fetch(base_url)
     assert response.code == 200
 
     with pytest.raises(tornado.httpclient.HTTPError, match='404'):
-        yield http_client.fetch(base_url + '/user/1')
+        await http_client.fetch(base_url + '/user/1')
 
-    response = yield http_client.fetch(base_url + '/user', method='POST',
+    response = await http_client.fetch(base_url + '/user', method='POST',
                                        body='name=fantix')
     assert response.code == 200
     assert b'fantix' in response.body
 
-    response = yield http_client.fetch(base_url + '/user/1')
+    response = await http_client.fetch(base_url + '/user/1')
     assert response.code == 200
     assert b'fantix' in response.body
 
-    response = yield http_client.fetch(base_url)
+    response = await http_client.fetch(base_url)
     assert response.code == 200
     assert b'fantix' in response.body
