bb73118563b00c7155aa36bac0540ca94e6d0fee
Fantix King <fantix.king@gmail.com>
2018-05-02 16:12:44 +0800

    Rename `relationships` to `extras`

--- gino/loader.py
+++ gino/loader.py
@@ -47,14 +47,14 @@ class Loader:
 
 
 class ModelLoader(Loader):
-    def __init__(self, model, *column_names, **relationships):
+    def __init__(self, model, *column_names, **extras):
         self.model = model
         if column_names:
             self.columns = [getattr(model, name) for name in column_names]
         else:
             self.columns = model
-        self.relationships = dict((key, self.get(value))
-                                  for key, value in relationships.items())
+        self.extras = dict((key, self.get(value))
+                           for key, value in extras.items())
         self.on_clause = None
 
     def do_load(self, row, context):
@@ -62,29 +62,29 @@ class ModelLoader(Loader):
         for c in self.columns:
             if c in row:
                 rv.__values__[c.name] = row[c]
-        for key, value in self.relationships.items():
+        for key, value in self.extras.items():
             setattr(rv, key, value.do_load(row, rv))
         return rv
 
     def get_columns(self):
         yield from self.columns
-        for subloader in self.relationships.values():
+        for subloader in self.extras.values():
             yield from subloader.get_columns()
 
     def get_from(self):
         rv = self.model
-        for key, subloader in self.relationships.items():
+        for key, subloader in self.extras.items():
             from_clause = subloader.get_from()
             if from_clause is not None:
                 rv = rv.outerjoin(from_clause,
                                   getattr(subloader, 'on_clause', None))
         return rv
 
-    def load(self, *column_names, **relationships):
+    def load(self, *column_names, **extras):
         if column_names:
             self.columns = [getattr(self.model, name) for name in column_names]
-        self.relationships.update((key, self.get(value))
-                                  for key, value in relationships.items())
+        self.extras.update((key, self.get(value))
+                           for key, value in extras.items())
         return self
 
     def on(self, on_clause):
@@ -93,8 +93,8 @@ class ModelLoader(Loader):
 
 
 class AliasLoader(ModelLoader):
-    def __init__(self, alias, *column_names, **relationships):
-        super().__init__(alias, *column_names, **relationships)
+    def __init__(self, alias, *column_names, **extras):
+        super().__init__(alias, *column_names, **extras)
 
 
 class ColumnLoader(Loader):
