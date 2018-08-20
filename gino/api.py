fb202feeaeabcefccb2672dc0182e230c3f6cb95
Tony Wang <wwwjfy@gmail.com>
2018-08-20 10:38:10 +0800

    minor fix, refs 3aef427

--- gino/api.py
+++ gino/api.py
@@ -503,4 +503,4 @@ class _PlaceHolder:
     def __setattr__(self, key, value):
         if key == '_exception':
             return super().__setattr__(key, value)
-        raise UninitializedError(self._exception)
+        raise self._exception
