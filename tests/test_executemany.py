cf73072caaa1b428b9d65c1f83feda0a5b6bac8b
Tony Wang <wwwjfy@gmail.com>
2018-08-26 13:52:13 +0800

    fix test cases

--- tests/test_executemany.py
+++ tests/test_executemany.py
@@ -20,34 +20,49 @@ async def test_status(bind):
 # noinspection PyUnusedLocal
 async def test_all(bind):
     result = await User.insert().returning(User.nickname).gino.all(
-        dict(nickname='1'), dict(nickname='2'))
+        dict(name='1'), dict(name='2'))
     assert result is None
-    assert len(await User.query.gino.all()) == 2
+    rows = await User.query.gino.all()
+    assert len(rows) == 2
+    assert set(u.nickname for u in rows) == {'1', '2'}
 
     result = await User.insert().gino.all(
-        dict(nickname='3'), dict(nickname='4'))
+        dict(name='3'), dict(name='4'))
     assert result is None
+    rows = await User.query.gino.all()
+    assert len(rows) == 4
+    assert set(u.nickname for u in rows) == {'1', '2', '3', '4'}
 
 
 # noinspection PyUnusedLocal
 async def test_first(bind):
     result = await User.insert().returning(User.nickname).gino.first(
-        dict(nickname='1'), dict(nickname='2'))
+        dict(name='1'), dict(name='2'))
     assert result is None
+    rows = await User.query.gino.all()
     assert len(await User.query.gino.all()) == 2
+    assert set(u.nickname for u in rows) == {'1', '2'}
 
     result = await User.insert().gino.first(
-        dict(nickname='3'), dict(nickname='4'))
+        dict(name='3'), dict(name='4'))
     assert result is None
+    rows = await User.query.gino.all()
+    assert len(rows) == 4
+    assert set(u.nickname for u in rows) == {'1', '2', '3', '4'}
 
 
 # noinspection PyUnusedLocal
 async def test_scalar(bind):
     result = await User.insert().returning(User.nickname).gino.scalar(
-        dict(nickname='1'), dict(nickname='2'))
+        dict(name='1'), dict(name='2'))
     assert result is None
+    rows = await User.query.gino.all()
     assert len(await User.query.gino.all()) == 2
+    assert set(u.nickname for u in rows) == {'1', '2'}
 
     result = await User.insert().gino.scalar(
-        dict(nickname='3'), dict(nickname='4'))
+        dict(name='3'), dict(name='4'))
     assert result is None
+    rows = await User.query.gino.all()
+    assert len(rows) == 4
+    assert set(u.nickname for u in rows) == {'1', '2', '3', '4'}
