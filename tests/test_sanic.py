7a1094a7b53ae3d4d1f4507ccfd15805888173b7
Fantix King <fantix.king@gmail.com>
2019-09-08 14:50:55 -0500

    fix Sanic test

--- tests/test_sanic.py
+++ tests/test_sanic.py
@@ -3,6 +3,11 @@ import sys
 
 from async_generator import yield_, async_generator
 import pytest
+
+# Sanic 19.6 dropped Python 3.5 support
+if sys.version_info < (3, 6):
+    raise pytest.skip(allow_module_level=True)
+
 import sanic
 from sanic.response import text, json
 
@@ -12,8 +17,6 @@ from gino.ext.sanic import Gino
 from .models import DB_ARGS, PG_URL
 
 _MAX_INACTIVE_CONNECTION_LIFETIME = 59.0
-pytestmark = pytest.mark.skipif(sys.version_info < (3, 6),
-                                reason="Sanic 19.6 dropped Python 3.5 support")
 
 
 def teardown_module():
