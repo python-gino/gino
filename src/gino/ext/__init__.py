887a9cd6c7a7ef1a35893490733b7f36d73ff58a
Aobo Shi <aobo.shi@decentfox.com>
2020-02-17 10:01:50 +0800

    Refs(#571):rename finder class

--- src/gino/ext/__init__.py
+++ src/gino/ext/__init__.py
@@ -1,11 +1,11 @@
 import sys
 from importlib.abc import MetaPathFinder
-from importlib.util import find_spec as _find_spec
+from importlib.util import find_spec
 
 from importlib_metadata import entry_points
 
 
-class DependencyFinder(MetaPathFinder):
+class PluginFinder(MetaPathFinder):
     def __init__(self):
         self.entry_points = {
             __name__ + "." + ep.name: ep.value
@@ -14,8 +14,8 @@ class DependencyFinder(MetaPathFinder):
 
     def find_spec(self, fullname, path, target=None):
         if fullname in self.entry_points:
-            spec = _find_spec(self.entry_points[fullname])
+            spec = find_spec(self.entry_points[fullname])
             return spec
 
 
-sys.meta_path.append(DependencyFinder())
+sys.meta_path.append(PluginFinder())
