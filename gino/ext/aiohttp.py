python-gino/gino

commit 5e5eabb7fc76ac1893a18f8bae54fe9691115807

fix #452, fix deprecated warnings
- text.bindparams from SQLAlchemy in asyncpg dialect
- loop from aiohttp in aiohttp extension

gino/ext/aiohttp.py
@@ -141,7 +141,6 @@ class Gino(_Gino):
                 min_size=config.setdefault('pool_min_size', 5),
                 max_size=config.setdefault('pool_max_size', 10),
                 ssl=config.setdefault('ssl'),
-                loop=app_.loop,
                 **config.setdefault('kwargs', dict()),
             )
 
@@ -157,6 +156,6 @@ class Gino(_Gino):
             raise HTTPNotFound(reason='No such data')
         return rv
 
-    async def set_bind(self, bind, loop=None, **kwargs):
+    async def set_bind(self, bind, **kwargs):
         kwargs.setdefault('strategy', 'aiohttp')
-        return await super().set_bind(bind, loop=loop, **kwargs)
+        return await super().set_bind(bind, **kwargs)

gino/dialects/asyncpg.py
@@ -309,7 +309,7 @@ class AsyncEnum(ENUM):
 
     async def _on_table_drop_async(self, target, bind, checkfirst=False, **kw):
         if not self.metadata and \
-            not kw.get('_is_metadata_operation', False) and \
+                not kw.get('_is_metadata_operation', False) and \
                 not self._check_for_name_in_memos(checkfirst, kw):
             await self.drop_async(bind=bind, checkfirst=checkfirst)
 
@@ -405,15 +405,16 @@ class AsyncpgDialect(PGDialect, base.AsyncDialectMixin):
         return val.upper()
 
     async def has_schema(self, connection, schema):
-        query = ("select nspname from pg_namespace "
-                 "where lower(nspname)=:schema")
         row = await connection.first(
             sql.text(
-                query,
-                bindparams=[
-                    sql.bindparam(
-                        'schema', util.text_type(schema.lower()),
-                        type_=sqltypes.Unicode)]
+                "select nspname from pg_namespace "
+                "where lower(nspname)=:schema"
+            ).bindparams(
+                sql.bindparam(
+                    'schema',
+                    util.text_type(schema.lower()),
+                    type_=sqltypes.Unicode,
+                )
             )
         )
 
@@ -427,10 +428,13 @@ class AsyncpgDialect(PGDialect, base.AsyncDialectMixin):
                     "select relname from pg_class c join pg_namespace n on "
                     "n.oid=c.relnamespace where "
                     "pg_catalog.pg_table_is_visible(c.oid) "
-                    "and relname=:name",
-                    bindparams=[
-                        sql.bindparam('name', util.text_type(table_name),
-                                      type_=sqltypes.Unicode)]
+                    "and relname=:name"
+                ).bindparams(
+                    sql.bindparam(
+                        'name',
+                        util.text_type(table_name),
+                        type_=sqltypes.Unicode
+                    ),
                 )
             )
         else:
@@ -438,14 +442,18 @@ class AsyncpgDialect(PGDialect, base.AsyncDialectMixin):
                 sql.text(
                     "select relname from pg_class c join pg_namespace n on "
                     "n.oid=c.relnamespace where n.nspname=:schema and "
-                    "relname=:name",
-                    bindparams=[
-                        sql.bindparam('name',
-                                      util.text_type(table_name),
-                                      type_=sqltypes.Unicode),
-                        sql.bindparam('schema',
-                                      util.text_type(schema),
-                                      type_=sqltypes.Unicode)]
+                    "relname=:name"
+                ).bindparams(
+                    sql.bindparam(
+                        'name',
+                        util.text_type(table_name),
+                        type_=sqltypes.Unicode,
+                    ),
+                    sql.bindparam(
+                        'schema',
+                        util.text_type(schema),
+                        type_=sqltypes.Unicode,
+                    )
                 )
             )
         return bool(row)
@@ -457,11 +465,13 @@ class AsyncpgDialect(PGDialect, base.AsyncDialectMixin):
                     "SELECT relname FROM pg_class c join pg_namespace n on "
                     "n.oid=c.relnamespace where relkind='S' and "
                     "n.nspname=current_schema() "
-                    "and relname=:name",
-                    bindparams=[
-                        sql.bindparam('name', util.text_type(sequence_name),
-                                      type_=sqltypes.Unicode)
-                    ]
+                    "and relname=:name"
+                ).bindparams(
+                    sql.bindparam(
+                        'name',
+                        util.text_type(sequence_name),
+                        type_=sqltypes.Unicode,
+                    )
                 )
             )
         else:
@@ -469,14 +479,18 @@ class AsyncpgDialect(PGDialect, base.AsyncDialectMixin):
                 sql.text(
                     "SELECT relname FROM pg_class c join pg_namespace n on "
                     "n.oid=c.relnamespace where relkind='S' and "
-                    "n.nspname=:schema and relname=:name",
-                    bindparams=[
-                        sql.bindparam('name', util.text_type(sequence_name),
-                                      type_=sqltypes.Unicode),
-                        sql.bindparam('schema',
-                                      util.text_type(schema),
-                                      type_=sqltypes.Unicode)
-                    ]
+                    "n.nspname=:schema and relname=:name"
+                ).bindparams(
+                    sql.bindparam(
+                        'name',
+                        util.text_type(sequence_name),
+                        type_=sqltypes.Unicode,
+                    ),
+                    sql.bindparam(
+                        'schema',
+                        util.text_type(schema),
+                        type_=sqltypes.Unicode,
+                    )
                 )
             )
 
@@ -503,12 +517,18 @@ class AsyncpgDialect(PGDialect, base.AsyncDialectMixin):
                 """
             query = sql.text(query)
         query = query.bindparams(
-            sql.bindparam('typname',
-                          util.text_type(type_name), type_=sqltypes.Unicode),
+            sql.bindparam(
+                'typname',
+                util.text_type(type_name),
+                type_=sqltypes.Unicode,
+            ),
         )
         if schema is not None:
             query = query.bindparams(
-                sql.bindparam('nspname',
-                              util.text_type(schema), type_=sqltypes.Unicode),
+                sql.bindparam(
+                    'nspname',
+                    util.text_type(schema),
+                    type_=sqltypes.Unicode,
+                ),
             )
         return bool(await connection.scalar(query))

commit c42de9410b0331f85478b66b70342be306fcc14e

fix #518, fix aiohttp set_bind incompatibility
parameter `loop` was removed in 5e5eabb, which broke the use of
with_bind

gino/ext/aiohttp.py
@@ -157,6 +157,6 @@ class Gino(_Gino):
             raise HTTPNotFound(reason="No such data")
         return rv
 
-    async def set_bind(self, bind, **kwargs):
+    async def set_bind(self, bind, loop=None, **kwargs):
         kwargs.setdefault("strategy", "aiohttp")
-        return await super().set_bind(bind, **kwargs)
+        return await super().set_bind(bind, loop=None, **kwargs)