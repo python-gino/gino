fe34606c226e2f3792692e8e96d1ea349fa70b4a
Tony Wang <wwwjfy@gmail.com>
2020-04-19 17:59:14 +0800

    Bump version: 0.8.6 → 0.8.7

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='gino',
-    version='0.8.6',
+    version='0.8.7',
     description="GINO Is Not ORM - "
                 "a Python asyncio ORM on SQLAlchemy core.",
     long_description=readme + '\n\n' + history,
