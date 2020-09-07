b365c67c6cabc5d7e06f0972cca1cdb63192f180
Fantix King <fantix.king@gmail.com>
2020-09-07 15:11:59 -0500

    set default level of logger "gino" to WARNING
    Fixes #710

--- src/gino/__init__.py
+++ src/gino/__init__.py
@@ -1,8 +1,14 @@
+import logging
+
 from .api import Gino  # NOQA
 from .bakery import Bakery
 from .engine import GinoEngine, GinoConnection  # NOQA
 from .exceptions import *  # NOQA
 from .strategies import GinoStrategy  # NOQA
+
+rootlogger = logging.getLogger("gino")
+if rootlogger.level == logging.NOTSET:
+    rootlogger.setLevel(logging.WARN)
 
 
 def create_engine(*args, **kwargs):
