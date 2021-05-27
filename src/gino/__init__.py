2254489f81a37548e31c503e413bc2e386788b41
Cobalt <chaosthe0rie@protonmail.com>
2021-05-27 12:13:05 +0200

    fix style [2]
    I really hate black sometimes

--- src/gino/__init__.py
+++ src/gino/__init__.py
@@ -15,18 +15,14 @@ if rootlogger.level == logging.NOTSET:
 def create_engine(*args, **kwargs):
     """
     Shortcut for :func:`sqlalchemy.create_engine` with ``strategy="gino"``.
-
     .. versionchanged:: 1.1
        Added the ``bakery`` keyword argument, please see :class:`~.bakery.Bakery`.
-
     .. versionchanged:: 1.1
        Added the ``prebake`` keyword argument to choose when to create the prepared
        statements for the queries in the bakery:
-
        * **Pre-bake** immediately when connected to the database (default).
        * No **pre-bake** but create prepared statements lazily when needed for the first
          time.
-
        Note: ``prebake`` has no effect in aiomysql
     """
 
@@ -60,7 +56,7 @@ except ValueError:
     # Unable to determine version leads to warning
     print("WARNING: Unable to determine current python version")
 
-    
+
 # noinspection PyBroadException
 try:
     __version__ = get_version()
